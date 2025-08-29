003d7f8f61bb9d595d2610f57e0e32f10061d723
Sumit Dongardiye <sumitdongardiye035@gmail.com>
2025-08-29 19:44:47 +0530

    day16

---
+++ Day 16/turtleLibrary.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy=Turtle()   # Created a new turtle object
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+
+my_screen=Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
